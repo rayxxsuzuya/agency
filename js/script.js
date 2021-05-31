409496be28a07b09b7285c08a7d2ba3a724799db
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-05-31 05:07:48 +0300

    about stylized

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+var tabs = document.querySelectorAll(".tabs");
+var contents = document.querySelectorAll(".contents");
+
+var _loop = function _loop(x) {
+  var _loop2 = function _loop2(i) {
+    tabs[x].children[i].addEventListener("click", function (e) {
+      e.preventDefault();
+
+      for (var j = 0; j < tabs[x].children.length; j++) {
+        tabs[x].children[j].classList.remove("active");
+        contents[x].children[j].classList.remove("active");
+      }
+
+      tabs[x].children[i].classList.add("active");
+      contents[x].children[i].classList.add("active");
+    });
+  };
+
+  for (var i = 0; i < tabs[x].children.length; i++) {
+    _loop2(i);
+  }
+};
+
+for (var x = 0; x < tabs.length; x++) {
+  _loop(x);
+}
