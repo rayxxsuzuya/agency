ab6a880768d12c7df6235d5171b9d470171eb15a
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-06-28 00:08:25 +0300

    other calendar stylized

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 "use strict";
 
+/* Slick slider */
+$(function () {
+  $(".calendar__slider").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+  });
+  $(".team__slider").slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true
+  });
+});
 /* Loader */
+
 window.onload = function () {
-  document.querySelector('.loader').style.display = 'none';
+  document.querySelector(".loader").style.display = "none";
 };
 /* Tabs */
 
@@ -36,88 +51,88 @@ for (var x = 0; x < tabs.length; x++) {
 /* Burger */
 
 
-var burger = document.querySelector('.header__burger');
-var mobile = document.querySelector('.header__mobile');
-burger.addEventListener('click', function (e) {
+var burger = document.querySelector(".header__burger");
+var mobile = document.querySelector(".header__mobile");
+burger.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if (burger.classList.contains('active')) {
-    burger.setAttribute('aria-label', 'Открыть меню');
+  if (burger.classList.contains("active")) {
+    burger.setAttribute("aria-label", "Открыть меню");
   } else {
     burger.setAttribute("aria-label", "Закрыть меню");
   }
 
   this.classList.toggle("active");
-  mobile.classList.toggle('active');
+  mobile.classList.toggle("active");
 });
 /* Nav focus */
 
-var navItem = document.querySelector('.header__item:nth-child(2) > .header__link');
-var subList = document.querySelector('.header__sublist');
-var subItem = document.querySelector('.header__subitem:last-child > .header__sublink');
-navItem.addEventListener('focus', function () {
-  subList.style.display = 'block';
+var navItem = document.querySelector(".header__item:nth-child(2) > .header__link");
+var subList = document.querySelector(".header__sublist");
+var subItem = document.querySelector(".header__subitem:last-child > .header__sublink");
+navItem.addEventListener("focus", function () {
+  subList.style.display = "block";
 });
-subItem.addEventListener('keydown', function (e) {
-  if (this == document.activeElement && e.key == 'Tab') {
-    subList.style.display = 'none';
+subItem.addEventListener("keydown", function (e) {
+  if (this == document.activeElement && e.key == "Tab") {
+    subList.style.display = "none";
   }
 });
-subList.addEventListener('blur', function (e) {
-  this.style.display = 'none';
+subList.addEventListener("blur", function (e) {
+  this.style.display = "none";
 });
-document.body.addEventListener('click', function () {
+document.body.addEventListener("click", function () {
   for (var i = 0; i < subList.children.length; i++) {
     if (subList.children[i] == document.activeElement) {
       return null;
     } else {
       subList.style.display = "none";
-      subList.removeAttribute('style');
+      subList.removeAttribute("style");
     }
   }
 });
 /* Modal */
 
-var loginButton = document.querySelector('.header__button');
-var loginButtonMobile = document.querySelector('.header__button--mobile');
-var loginModal = document.querySelector('.modal-login');
-var registerModal = document.querySelector('.modal-register');
-var registerButton = document.querySelector('.modal__register');
-loginButton.addEventListener('click', function (e) {
+var loginButton = document.querySelector(".header__button");
+var loginButtonMobile = document.querySelector(".header__button--mobile");
+var loginModal = document.querySelector(".modal-login");
+var registerModal = document.querySelector(".modal-register");
+var registerButton = document.querySelector(".modal__register");
+loginButton.addEventListener("click", function (e) {
   e.preventDefault();
-  loginModal.classList.toggle('active');
-  document.querySelector('.modal__area').focus();
+  loginModal.classList.toggle("active");
+  document.querySelector(".modal__area").focus();
 });
 loginButtonMobile.addEventListener("click", function (e) {
   e.preventDefault();
   loginModal.classList.toggle("active");
   document.querySelector(".modal__area").focus();
 });
-registerButton.addEventListener('click', function (e) {
+registerButton.addEventListener("click", function (e) {
   e.preventDefault();
   loginModal.classList.remove("active");
   registerModal.classList.add("active");
-  document.querySelector('.modal-register .modal__area').focus();
+  document.querySelector(".modal-register .modal__area").focus();
 });
-registerButton.addEventListener('keydown', function (e) {
+registerButton.addEventListener("keydown", function (e) {
   e.preventDefault();
 
-  if (e.key == 'Tab' && this == document.activeElement) {
+  if (e.key == "Tab" && this == document.activeElement) {
     modalClose[0].focus();
-  } else if (e.key == 'Enter') {
-    registerModal.classList.add('active');
-    loginModal.classList.remove('active');
+  } else if (e.key == "Enter") {
+    registerModal.classList.add("active");
+    loginModal.classList.remove("active");
     document.querySelector(".modal-register .modal__area").focus();
   }
 });
-document.querySelector('.modal-register__button').addEventListener("keydown", function (e) {
+document.querySelector(".modal-register__button").addEventListener("keydown", function (e) {
   e.preventDefault();
 
   if (e.key == "Tab" && this == document.activeElement) {
     modalClose[1].focus();
   } else if (e.key == "Enter") {}
 });
-var modalClose = document.querySelectorAll('.modal__close');
+var modalClose = document.querySelectorAll(".modal__close");
 modalClose[0].addEventListener("click", function (e) {
   e.preventDefault();
   loginModal.classList.remove("active");
@@ -127,13 +142,13 @@ modalClose[1].addEventListener("click", function (e) {
   registerModal.classList.remove("active");
 });
 modalClose[0].addEventListener("keydown", function (e) {
-  if (e.key == 'Tab' && this == document.activeElement) {
-    document.querySelector('.modal__area--first').focus();
+  if (e.key == "Tab" && this == document.activeElement) {
+    document.querySelector(".modal__area--first").focus();
   }
 });
 /* About Slider */
 
-var slides = document.querySelectorAll('.about__contents .about__content');
+var slides = document.querySelectorAll(".about__contents .about__content");
 var currentSlide = 0;
 
 function nextSlide() {
@@ -145,13 +160,13 @@ function previousSlide() {
 }
 
 function goToSlide(n) {
-  slides[currentSlide].classList.remove('active');
+  slides[currentSlide].classList.remove("active");
   currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].classList.add('active');
+  slides[currentSlide].classList.add("active");
 }
 
-var next = document.querySelector('.about__next');
-var previous = document.querySelector('.about__prev');
+var next = document.querySelector(".about__next");
+var previous = document.querySelector(".about__prev");
 
 next.onclick = function () {
   nextSlide();
@@ -160,11 +175,3 @@ next.onclick = function () {
 previous.onclick = function () {
   previousSlide();
 };
-/* Slick slider */
-
-
-$(".team__slider").slick({
-  slidesToShow: 1,
-  arrows: false,
-  dots: true
-});
